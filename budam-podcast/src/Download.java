237396568f2e99cc2a8070c56f557a7ac3d8e490
AveVlad <dev@vld.me>
2014-04-22 02:35:09 +0400

    fix class

--- budam-podcast/src/Download.java
+++ budam-podcast/src/Download.java
@@ -1,13 +1,14 @@
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Download {
     public static void main(String[] args) throws IOException {
         Download dwn = new Download();
-        for (int i = 1; i <= 422; i++) {
-            dwn.saveUrl("http://myflex.org/yf/podru/budam" + i +".mp3");
+        for (int i = 160; i <= 422; i++) {
+            dwn.saveUrl("http://myflex.org/yf/podru/budam" + i + ".mp3");
         }
     }
 
@@ -19,10 +20,16 @@ public class Download {
         try {
             System.out.println("get " + urlString);
             site = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection)site.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            if (connection.getResponseCode() != 200) {
+                System.out.println(urlString + " not found");
+                return;
+            }
             in = new BufferedInputStream(site.openStream());
             filename = site.getFile();
             fout = new FileOutputStream("C:" + site.getFile());
-
             final byte data[] = new byte[1024];
             int count;
             while ((count = in.read(data, 0, 1024)) != -1) {
